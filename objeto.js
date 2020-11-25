7cd1b0b813e718bd78a0c22ecb452a668495c42b
LinceIberico <61143255+LinceIberico@users.noreply.github.com>
2020-11-25 18:26:52 +0100

    Se ha añadido las clases leve y grave

--- objeto.js
+++ objeto.js
@@ -3,15 +3,64 @@
 class Multa {
 
     constructor(idMulta, nifConductor, nifGuardia, importe, pagada, descripcion, fecha) {
-        this.idMulta = idMulta;
-        this.nifConductor = nifConductor;
-        this.nifGuardia = nifGuardia;
-        this.importe = importe;
-        this.pagada = pagada;
-        this.descripcion = descripcion;
-        this.fecha = fecha;
-
+        this._idMulta = idMulta;
+        this._nifConductor = nifConductor;
+        this._nifGuardia = nifGuardia;
+        this._importe = importe;
+        this._pagada = pagada;
+        this._descripcion = descripcion;
+        this._fecha = fecha;
     }
+    get idMulta() {
+        return this._idMulta;
+    }
+    set idMulta(valor) {
+        this._idMulta = valor;
+    }
+
+    get nifConductor() {
+        return this._nifConductor;
+    }
+    set nifConductor(valor) {
+        this._nifConductor = valor;
+    }
+
+    get nifGuardia() {
+        return this._nifGuardia;
+    }
+    set nifGuardia(valor) {
+        this._nifGuardia = valor;
+    }
+
+    get importe() {
+        return this._importe;
+    }
+    set importe(valor) {
+        this._importe = valor;
+    }
+
+    get pagada() {
+        return this._pagada;
+    }
+    set pagada(valor) {
+        this._pagada = valor;
+    }
+
+    get descripcion() {
+        return this._descripcion;
+    }
+    set descripcion(valor) {
+        this._descripcion = valor;
+    }
+
+    get fecha() {
+        return this._fecha;
+    }
+    set fecha(valor) {
+        this._fecha = valor;
+    }
+
+
     listarMulta() {
         //Creamos el listado que contiene el documento de la MULTA
         let tabla = '<table border="1">';
@@ -25,4 +74,22 @@ class Multa {
     }
 
 
+}
+
+//Clase leve
+
+class Leve extends Multa {
+
+    constructor(idMulta, nifConductor, nifGuardia, importe, pagada, descripcion, fecha, bonificada) {
+        super(idMulta, nifConductor, nifGuardia, importe, pagada, descripcion, fecha);
+        this.bonificada = bonificada;
+    }
+}
+//Clase Grave
+
+class Grave extends Multa {
+    constructor(idMulta, nifConductor, nifGuardia, importe, pagada, descripcion, fecha, puntos) {
+        super(idMulta, nifConductor, nifGuardia, importe, pagada, descripcion, fecha);
+        this.puntos = puntos;
+    }
 }
